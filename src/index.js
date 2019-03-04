6a559fadd34b7107333d80041fa82399d36a76ef
Anastasia Zabela <zabela113@mail.ru>
2019-03-04 18:38:30 +0300

    feat: calculates how many love triangles phenomenons take place

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0; //number of love triangles
+  let double = []; //arr of double number
+  for (let i=0; i < preferences.length; i++) {
+    let a = i+1; //current index of element
+    let b = preferences[i]; //element
+    if (double.indexOf(a) == -1 && a != b) {
+      for (let j=0; j < preferences.length; j++) {
+        if (b === j+1) {
+          for (let k=0; k < preferences.length; k++) {
+            if (preferences[j] === k+1) {
+              if (preferences[k] === a) {
+                double.push(i+1, j+1, k+1);
+                count +=1; 
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return count;
 };
