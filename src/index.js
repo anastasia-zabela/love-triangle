e10ef923482693a676ae582f8b5915eb70b30209
Anastasia Zabela <zabela113@mail.ru>
2019-03-21 22:09:50 +0300

    style: rename variables

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  let count = 0; //number of love triangles
-  let double = []; //arr of double number
-  for (let i=0; i < preferences.length; i++) {
-    let a = i+1; //current index of element
-    let b = preferences[i]; //element
-    if (double.indexOf(a) == -1 && a != b) {
-      for (let j=0; j < preferences.length; j++) {
-        if (b === j+1) {
-          for (let k=0; k < preferences.length; k++) {
-            if (preferences[j] === k+1) {
-              if (preferences[k] === a) {
-                double.push(i+1, j+1, k+1);
-                count +=1; 
+  let countTriangles = 0; 
+  let numbersRepeating = [];
+  for (let i=0; i<preferences.length; i++) {
+    let firstSpichonee = i+1;
+    let loveFirstSpichonee = preferences[i];
+    if (numbersRepeating.indexOf(firstSpichonee) == -1 && firstSpichonee != loveFirstSpichonee) {
+      for (let j=0; j<preferences.length; j++) {
+        let twiceSpichonee = j+1;
+        if (loveFirstSpichonee === twiceSpichonee) {
+          for (let k=0; k<preferences.length; k++) {
+            let thirdSpichonee = k+1;
+            if (preferences[j] === thirdSpichonee) {
+              if (preferences[k] === firstSpichonee) {
+                numbersRepeating.push(i+1, j+1, k+1);
+                countTriangles += 1; 
               }
             }
           }
@@ -23,5 +25,5 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
       }
     }
   }
-  return count;
+  return countTriangles;
 };
